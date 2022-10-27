10fa2f84271abda7b0dbfa1bc0f116d7912d202a
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-10-27 17:19:44 -0300

    trab

--- flask_auth_scotch-master/project/main.py
+++ flask_auth_scotch-master/project/main.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-
+#from timer.py import pomodoro
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -13,3 +13,7 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+#export FLASK_APP=project
+#export FLASK_DEBUG=1
--- flask_auth_scotch-master/project/models.py
+++ flask_auth_scotch-master/project/models.py
@@ -8,3 +8,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+    
