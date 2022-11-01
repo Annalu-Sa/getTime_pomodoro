77558d8316eee413f52da0cdcbcae31dcb159295
kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-11-01 13:26:47 -0300

    coments

--- flask_auth_scotch-master/project/main.py
+++ flask_auth_scotch-master/project/main.py
@@ -2,18 +2,21 @@
 
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-#from timer.py import pomodoro
+
+
 main = Blueprint('main', __name__)
 
+# Temos dois blueprints para as nossas rotas
+# No blueprint principal teremos a página inicial com a rota '/' e a página de usuário com a rota '/profile'
+# Se o usuário tentar acessar sem ter conta(ser autenticado) ele será encaminhado para a rota de login 
+
+# Depois da página inicial, o usuário é encaminhado para a página do usuário
 @main.route('/')
 def index():
     return render_template('index.html')
 
 @main.route('/profile')
-@login_required
+@login_required #função para exigir que o usuário tenha login
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-
-#export FLASK_APP=project
-#export FLASK_DEBUG=1
